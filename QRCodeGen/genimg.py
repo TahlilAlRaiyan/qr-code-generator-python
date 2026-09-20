06e471b77073f50ec13547694ca0ed36c312dea3
Mohammad Tahlil Al-Raiyan <raiyans525@gmail.com>
2026-09-20 09:08:50 +0600

    Add files via upload

---
+++ QRCodeGen/genimg.py
@@ -0,0 +1,15 @@
+import qrcode
+
+url = input("Enter the URL to generate QR code: ")
+
+file_path = "C:\\Users\\raiya\\Desktop\\qrcode.png"
+
+qr = qrcode.QRCode()
+
+qr.add_data(url)
+
+img = qr.make_image()
+
+img.save(file_path)
+
+print("QR code generated and saved to:", file_path)
